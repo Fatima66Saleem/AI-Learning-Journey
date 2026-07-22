699f4ca0ec18982d8393c7ca14c00c8a5cd93d97
Fatima Saleem <fatimaasalim87@gmail.com>
2026-07-22 10:25:24 +0500

    Day 1: print statement and escape sequences practice

---
+++ Day01_Python_Basics/first_program.py
@@ -0,0 +1,27 @@
+print("Hello World") #print is a function of python
+print("My name is: \nFatima") #\n is for new line
+
+# To make quotes appear literally in the output,
+# wrap the string in single quotes and use double quotes inside
+print('"Hello"')
+
+# Alternative: use backslash to escape the double quotes
+print("\"Hello\"")
+
+print("")  #empty string
+
+print("Name:\tFatima")  # \t = tab space
+
+
+# \b is the backspace escape sequence
+# it moves the cursor one step back (like pressing Backspace)
+# not commonly used, but useful to recognize
+print("Fatima\b")
+
+# common escape sequences for reference:
+print("Line1\nLine2")     # \n = new line
+print("Name:\tFatima")    # \t = tab space
+print("She said \"Hi\"")  # \" = double quote inside string
+print('It\'s mine')       # \' = single quote inside string
+print("C:\\Users\\Fatima") # \\ = single backslash
+
